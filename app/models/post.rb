6472da4a7cb8422866eb1ac1ed226b4c532119e1
Stephen Crane <scrane88@gmail.com>
2021-09-28 09:51:42 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,12 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-
+  def categories_attributes=(categories_attributes)
+    categories_attributes.each do |_k, category_attributes|
+      if category_attributes[:name].strip != ""
+        category = Category.find_or_create_by(category_attributes)
+        categories << category
+      end
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create(name: "Meditation")
+Category.create(name: "Coding")
+Category.create(name: "Health")
+Category.create(name: "Gaming")
